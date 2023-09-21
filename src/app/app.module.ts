82178f7eb5a4b75d96f155a54b1b77739f448981
smendescoti <sergio.coti@gmail.com>
2023-09-20 22:05:16 -0300

    feat: criação das páginas de produtos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,25 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/layout/navbar/navbar.component';
+import { CadastroProdutosComponent } from './components/pages/cadastro-produtos/cadastro-produtos.component';
+import { ConsultaProdutosComponent } from './components/pages/consulta-produtos/consulta-produtos.component';
+import { EdicaoProdutosComponent } from './components/pages/edicao-produtos/edicao-produtos.component';
+import { AppRoutingModule } from './app.routing';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CadastroProdutosComponent,
+    ConsultaProdutosComponent,
+    EdicaoProdutosComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule, //adicionando a configurações das rotas do projeto
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
